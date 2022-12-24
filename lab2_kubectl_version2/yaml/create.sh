8414bb29cf68a5852dbb1c36b076bb2c86c67f59
Elizalde Esteban <zaldy1028@gmail.com>
2022-12-24 12:41:21 +0800

    load balancer output

--- lab2_kubectl_version2/yaml/create.sh
+++ lab2_kubectl_version2/yaml/create.sh
@@ -4,7 +4,7 @@ kubectl create -f backend.yaml
 kubectl create -f frontend-configmap.yaml
 kubectl create -f frontend-service.yaml
 kubectl create -f frontend.yaml
-kubectl create -f ingress.yaml
+# kubectl create -f ingress.yaml
 kubectl create -f mongodb-persistent-volume.yaml
 kubectl create -f mongodb-persistent-volume-claim.yaml
 kubectl create -f mongodb-secret.yaml
